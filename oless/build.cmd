2429314193350afd19e3c074ba13539cf322e9d6
DBHeise <david@heiseink.com>
2024-09-09 07:36:37 -0600

    Fixes #1

--- oless/build.cmd
+++ oless/build.cmd
@@ -1,1 +1,8 @@
-msbuild .\oless.vcxproj /t:Rebuild /p:Configuration=Debug;OutDir=..\Debug
+
+for %%x in (msbuild) do if not [%%~$PATH:x]=[] set HasMSBuild=1
+
+if NOT DEFINED HasMSBuild (
+	cl.exe /Zi /EHsc /std:c++14 /Fe:oless pole.cpp olessoffice.cpp oless.cpp vbahelper.cpp program.cpp
+) ELSE (
+	msbuild .\oless.vcxproj /t:Rebuild /p:Configuration=Debug;OutDir=..\Debug
+)
